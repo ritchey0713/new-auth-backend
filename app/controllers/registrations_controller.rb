31862b2ad4156844ba9c6e1955a61708df5d1905
ritchey0713 <ritchey0713@gmail.com>
2020-11-04 13:18:43 -0500

    flesh out auth

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController 
+
+    def create
+      @user = User.create(email: params[:email], password: params["password"], password_confirmation: params["password_confirmation"])
+
+      if @user 
+        session[:user_id] = @user.id
+        render json: {
+          status: :created,
+          user: @user
+        }
+      else
+        rennder json: {
+          status: 500
+        }
+      end 
+    end 
+
+end 
